25c70baa03089644d2e70e7c95c331839c2cc2a2
Hunter <fleakhunter@gmail.com>
2020-10-28 13:54:01 -0400

    commit tree 66c5d3f3618e88120d4ae648d85e6ef443dfcf43

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+require "pry"
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.map do |attendee|
+   badge_maker(attendee)
+  end
+end
+
+def assign_rooms(attendees)
+  final_array = []
+  attendees.each_with_index do |attendee, index|
+    final_array.push("Hello, #{attendee}! You'll be assigned to room #{index+1}!")
+  end
+  return final_array
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+
+  assign_rooms(attendees).each do |assignment|
+    puts assignment
+  end
+end
